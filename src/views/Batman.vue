3f598b916dc84e9782032c7d06e8f9e57db6c217
Engin Gokmen <engin.gokmen@icloud.com>
2019-12-11 14:28:15 +0300

    summary fetch added, unused codes are deleted

--- src/views/Batman.vue
+++ src/views/Batman.vue
@@ -1,15 +1,38 @@
 <template>
   <div class="batman">
     <h1>SUMMARY</h1>
-    <p>
-      {{ id }}
-    </p>
+    <div>
+      <img :src="summary.data.image.original" />
+      <p>
+        Genres
+      </p>
+      <ul v-for="genre in summary.data.genres" :key="genre.id">
+        <li>
+          {{ genre }}
+        </li>
+      </ul>
+      <p>Premiered: {{ summary.data.premiered }}</p>
+      <p>Rating: Average {{ summary.data.rating.average }}</p>
+      <p v-html="summary.data.summary"></p>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "batman",
-  props: ["id"]
+  props: ["id"],
+  data() {
+    return {
+      summary: null
+    };
+  },
+  mounted() {
+    axios
+      .get("http://api.tvmaze.com/shows/" + this.id)
+      .then(response => (this.summary = response));
+  }
 };
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,9 @@
 <template>
   <div class="home">
-    <router-link :to="{ name: 'batman', params: {id:info.data[0]['show']['id']} }">LINK</router-link>
-    <!-- {{info.data[0]['show']['image']['medium']}} -->
-    <!-- {{info.data[0]['show']['id']}} -->
-
-    <!-- {{ info.data }} -->
-    <!-- {{arrayOfImages}} -->
     <div v-for="each in info.data" :key="each.id">
-      <!-- <p> -->
-        <!-- {{each["show"]["id"]}} -->
-        <!-- {{each["show"]["image"]["medium"]}} -->
-      <!-- </p> -->
-      <router-link :to="{ name:'batman', params: {id:each.show.id}}"><img :src="each.show.image.medium" /></router-link>
+      <router-link :to="{ name: 'batman', params: { id: each.show.id } }">
+        <img :src="each.show.image.medium"
+      /></router-link>
     </div>
   </div>
 </template>
@@ -21,28 +13,14 @@ import axios from "axios";
 // @ is an alias to /src
 export default {
   name: "home",
-  components: {},
   data() {
     return {
       source: "http://api.tvmaze.com/search/shows?q=batman",
-      info: null,
-      arrayOfImages: []
+      info: null
     };
   },
   mounted() {
-    axios.get(this.source)
-      .then(response => (this.info = response))
-      .then(this.imagesToArray);
-
-  },
-  filters: {},
-  computed: {},
-  methods: {
-    imagesToArray: function() {
-      for (const element of this.info.data) {
-        this.arrayOfImages.push(element["show"]["image"]["medium"]);
-      }
-    }
+    axios.get(this.source).then(response => (this.info = response));
   }
 };
 </script>
